b50dda35cbe8ca53e4920bd987b81e8f47049033
Alan Heuertz <aheuertz@asu.edu>
2011-09-27 16:26:48 -0700

    working/limited prototype testing the interweaving of ChickenFarm and Retailer threads

--- ChickenFarmModel/ChickenFarmModel/Program.cs
+++ ChickenFarmModel/ChickenFarmModel/Program.cs
@@ -8,46 +8,119 @@ namespace ChickenFarmModel
 {
     public delegate void priceCutEvent(Int32 price);
 
+
     class Program
     {
-        private static Int32 NUM_RETAILERS = 5;
+        public static Int32 NUM_RETAILERS = 5;
+        private static Thread farmer;
 
         static void Main(string[] args)
         {
+            Random randNum = new Random();
+            bool childIsAlive = true;
+            /* TIME STAMP STUFF, MOVE LATER */
+            /* TIME STAMP STUFF, MOVE LATER */
+            DateTime startTime = new DateTime();
+            DateTime endTime = new DateTime();
+            TimeSpan span = endTime.Subtract(startTime);
+            /* TIME STAMP STUFF, MOVE LATER */
+            /* TIME STAMP STUFF, MOVE LATER */
+
             ChickenFarm farmRamRod = new ChickenFarm();
-            Thread farmer = new Thread(new ThreadStart(farmRamRod.farmerFunc));
+            farmer = new Thread(new ThreadStart(farmRamRod.farmerFunc));
             farmer.Start();         // Start one farmer thread
-            Retailer chickenstore = new Retailer();
+            Retailer chickenstore = new Retailer(-1);
             ChickenFarm.priceCut += new priceCutEvent(chickenstore.chickenOnSale);
             Thread[] retailers = new Thread[NUM_RETAILERS];
             for (int i = 0; i < NUM_RETAILERS; i++)
             {   // Start N retailer threads
-                retailers[i] = new Thread(new ThreadStart(chickenstore.retailerFunc));
+                retailers[i] = new Thread(new ThreadStart(new Retailer(i).retailerFunc));
                 retailers[i].Name = (i + 1).ToString();
                 retailers[i].Start();
             }
 
+            while (childIsAlive)
+            {
+                Thread.Sleep(randNum.Next(1, 100));
+
+                childIsAlive = false;
+                for (int i = 0; i < retailers.Length; i++)
+                {
+                    if (retailers[i].IsAlive) childIsAlive = true;
+                }
+            }
             // farmer.Join(); // is this necessary?
             // For debugging purposes... TEST THIS STUFF
             // Should retailers join first?
             
         }
+
+        public static bool ChickenFarmIsAlive()
+        {
+            return farmer.IsAlive;
+        }
     }
 
     class Retailer
     {
+        public static bool[] priceWasCutArray;
+        bool priceWasCut = false;
+        Int32 myId;
+
+        public Retailer(int id){
+            myId = id;
+            priceWasCutArray = new bool[Program.NUM_RETAILERS];
+            for(int i = 0; i < Program.NUM_RETAILERS; i++){
+                priceWasCutArray[i] = true;
+            }
+        }
+
+        public Int32 getId()
+        {
+            return myId;
+        }
+
         // for starting thread
         public void retailerFunc()
         {
-            // Possibly put the priceCut += priceCutEvent here instead of Main
-            // ChickenFarm.getChickenPrice();
+            while (Program.ChickenFarmIsAlive())
+            {
+                Thread.Sleep(100);
+                //if(getId() == 1)
+                //System.Console.WriteLine("Retailers {0} starts loop", getId());
+                if (priceWasCutArray[getId()])
+                {
+                    priceWasCut = false;
+                    Console.WriteLine("Retailer {0} changed the bool to {1}.", getId(), priceWasCut);
+                }
+                //if(getId() == 1)
+                //System.Console.WriteLine("Retailers {0} ends loop", getId());
+                // Possibly put the priceCut += priceCutEvent here instead of Main
+                // ChickenFarm.getChickenPrice();
+
+
+                //System.Random randNum = new System.Random();
+
+//                while (!OrderBuffer.setCell(orderString))
+//                {
+                    // Is this the wait we need?
+//                    Thread.Sleep(randNum.Next(1, 100));
+//                }
+            }
         }
 
         // event handler for when the price is cut
         public void chickenOnSale(Int32 newPrice)
         {
             // TODO: evaluate price and order
-
+            //lock (priceWasCutArray)
+            //{
+                //for (int i = 0; i < priceWasCutArray.Length; i++)
+                //{
+                    priceWasCut = true;
+                //}
+                //Console.WriteLine("BOOL CHANGED TO: {0}", priceWasCut);
+            //}
             // NEED A TIMESTAMP WHEN SUBMITTED
             // TIME PASSED WILL BE CALCUALTED AND SAVED
             // SHOULD SECOND PART OF THIS TRANSACTION OCCUR IN THE ORDERPROCESSING THREAD?
@@ -72,13 +145,43 @@ namespace ChickenFarmModel
         {
             //NEED A TIME STAMP FOR BEGINNING/END
 
-            for (Int32 i = 0; i < 50; i++)
+            while (priceCutCounter < 10)
             {
-                Thread.Sleep(500); //?
+                System.Console.WriteLine("ChickenFarm starts and sleeps");
+                Thread.Sleep(100); //?
+                System.Console.WriteLine("ChickenFarm wakes up");
 
+
+
+                // PUT THIS IN THE TIMER WITH CHICKEN COUNTER
+                // PUT THIS IN THE TIMER WITH CHICKEN COUNTER
+                numChickens++;
                 Int32 newPrice = PricingModel.reevaluatePrice();
                 ChickenFarm.changePrice(newPrice);
+                // PUT THIS IN THE TIMER WITH CHICKEN COUNTER
+                // PUT THIS IN THE TIMER WITH CHICKEN COUNTER
+                /*
+                // check buffer code
+                lock (OrderBuffer.buffer) //double check this
+                {
+                    if (!OrderBuffer.IsEmpty())
+                    {
+                        for (int i = 0; i < Program.NUM_RETAILERS; i++)
+                        {
+                            // CHANGE THIS TO CREATE AN ORDER PROCESSING THREAD!
+                            System.Console.WriteLine(Decoder.decode(OrderBuffer.Consume(i)));
+                            // CHANGE THIS TO CREATE AN ORDER PROCESSING THREAD!
+                        }
+                    }
+                }
+                 */
+
+                System.Console.WriteLine("ChickenFarm ends iteration");
             }
+
+            // do not wait for OrderProcessing threads to terminate! This is a poor design
+            // choice if this software was implemented in real life, but for the sake of
+            // finishing the assignment on time, we will not deal with it.
         }
 
         // Changes the price according to the PricingModel and emits the event callback
@@ -91,6 +194,7 @@ namespace ChickenFarmModel
                 priceCutCounter++;
                 if (priceCut != null)
                 {
+                    Console.WriteLine("Price cut event executed!");
                     priceCut(price);
                 }
                 // IF priceCutCounter == 10, TERMINATE THREAD
@@ -149,6 +253,8 @@ namespace ChickenFarmModel
         private Int32 amount;
         private Int32 unitPrice;
 
+        
+
         public Int32 getThreadId(){ return threadId; }
         public Int32 getCardNum(){ return cardNum; }
         public Int32 getAmount(){ return amount; }
@@ -157,13 +263,51 @@ namespace ChickenFarmModel
         public void setCardNum(int cardNum){ this.cardNum = cardNum; }
         public void setAmount(int amount){ this.amount = amount; }
         public void setUnitPrice(Int32 unitPrice){ this.unitPrice = unitPrice; }
-        public String ToString(){ return threadId + ";" + cardNum + ";" + amount; }
+        public override String ToString(){ return threadId + ";" + cardNum + ";" + amount; }
     }
 
     // Multicell buffer that deals with Retailers and the ChickenFarm thread trying to
     // access it at the same time.
     class OrderBuffer
     {
+        public static String[] buffer;
+        bool isFull = false;
 
+        public OrderBuffer(){
+            buffer = new String[Program.NUM_RETAILERS];
+            for (int i = 0; i < Program.NUM_RETAILERS; i++)
+            {
+                buffer[i] = null;
+            }
+        }
+
+        public bool setCell(String s)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (buffer[i] == null)
+                {
+                    buffer[i] = s;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsEmpty()
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (buffer[i] != null) return false;
+            }
+            return true;
+        }
+
+        public static string Consume(int index)
+        {
+            string s = buffer[index];
+            buffer[index] = null;
+            return s;
+        }
     }
 }
